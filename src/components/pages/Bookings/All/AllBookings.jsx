7afbf052512b52074535bc9a49981bf2b63dda16
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2024-01-28 14:37:19 -0500

    Fixing code

--- src/components/pages/Bookings/All/AllBookings.jsx
+++ src/components/pages/Bookings/All/AllBookings.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import AvailableRooms from '../../../forms/AvailableRooms';
+import RoomCard from '../../../roomscards/RoomCard';
 
 
 const AllBookings = () => {
     return (
         <div>
             <NavBar/>
-            
+            <AvailableRooms/>
+            <RoomCard/>
         </div>
     );
 }
