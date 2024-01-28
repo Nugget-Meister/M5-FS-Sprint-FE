7b3c209f457fc8fc166c3defc170fa39bab431d2
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2024-01-28 14:11:10 -0500

    Adds Booking card and css

---
+++ src/components/roomscards/BookingCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./BookingCard.css"
+
+export default function BookingCard() {
+  return (
+    <div className='BookingCard'>
+        <h>Booking Name</h>
+        <p>Meeting Room Name</p>
+        <p>Start Time</p>
+        <p>End Time</p>
+        <p>Floor #</p>
+    </div>
+  )
+}
