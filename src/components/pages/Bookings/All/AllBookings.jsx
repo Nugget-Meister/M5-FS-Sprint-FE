3da199650c8d4a40205b3b1b191e882b54151096
Nugget-Meister <tremainecamacho@gmail.com>
2024-01-30 20:31:38 -0500

    begin bookings, proto api calls for getAll

--- src/components/pages/Bookings/All/AllBookings.jsx
+++ src/components/pages/Bookings/All/AllBookings.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import NavBar from "../../../NavBar";
-import BookingCard from "../../../roomscards/BookingCard";
+import { useState, useEffect } from "react";
+import { getAllBookings, getSingleBooking } from "../../../shared/apicallsbooking";
+import BookingCardS from "../subcomponents/BookingCardS";
+
 
 const AllBookings = () => {
-    const dummyData = new Array(3).fill("True");
-    console.log(dummyData);
+
+    const [bookings, setBookings] = useState([''])
+    const [rooms, setRooms] = useState([''])
+
+    useEffect(() => {
+
+        getAllBookings()
+        .then(json => {
+            console.log(json)
+            setBookings(json.data)
+        })
+        
+      
+        }, [])
+    
+    // const dummyData = new Array(3).fill("True");
+    console.log(bookings);
 
   return (
     <div>
       <div>
-        <h1>Bookings List:</h1>
-        {dummyData.map((card) => {
-          return <BookingCard />;
+        {/* <h1>Bookings List:</h1> */}
+        {bookings.map((card) => {
+          return <BookingCardS data={card}/>;
         })}
       </div>
     </div>
